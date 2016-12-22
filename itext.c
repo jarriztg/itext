511a928c5abe36d80884c9e38fc72890f3d44aa2
Harry <jarriztg@users.noreply.github.com>
2016-12-21 23:00:53 -0600

    Update itext.c

--- itext.c
+++ itext.c
@@ -5,33 +5,48 @@
 #include "formats.h"
 
 int main(int n, char *texto[]) {
+  char *ERROR[2];
+  ERROR[1] = "A text required.";
+  ERROR[2] = "A argument required, use itext -h to help you.";
+
   if (n>1) {
+    char *se_inicio = strstr(texto[1], "-");
+    if (se_inicio) {
 
-    if (!strcmp(texto[1], "-u")) {
-      if (n>2) {
-        underlined(texto[2]);
-      } else {
-        print("A text required");
+      if (strstr(texto[1], "ui")) {
+        if (n>2) {
+          italics_underlined(texto[2]);
+        } else {
+          print("A text required");
+        };
       };
-    };
 
-  if (!strcmp(texto[1], "-i")) {
-    if (n>2) {
-      italics(texto[2]);
-    } else {
-      print("A text required");
-    };
-  }
+      if (strstr(texto[1], "u")) {
+        if (n>2) {
+          underlined(texto[2]);
+        } else {
+          print("A text required");
+        };
+      };
 
-  if (!strcmp(texto[1], "-c")) {
-    if (n>2) {
-      circles(texto[2]);
-    } else {
-      print("A text required");
-    };
-  }
+      if (strstr(texto[1], "i")) {
+        if (n>2) {
+          italics(texto[2]);
+        } else {
+          print("A text required");
+        };
+      };
 
+      if (strstr(texto[1], "c")) {
+        if (n>2) {
+          circles(texto[2]);
+        } else {
+          print("A text required");
+        };
+      };
+
+  };
   } else {
-    print("A argument required, use itext -h to help you.");
+    print(ERROR[2]);
   };
 }
